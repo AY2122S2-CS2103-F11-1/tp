ed0e746cea85b2aee83708db6069715eab1e008e
dannytayjy <danny_tayjunyang@hotmail.com>
2022-04-11 01:22:44 +0800

    Add test cases

--- src/test/java/manageezpz/logic/commands/DeleteTaskCommandTest.java
+++ src/test/java/manageezpz/logic/commands/DeleteTaskCommandTest.java
@@ -1,17 +1,19 @@
 package manageezpz.logic.commands;
 
-import static manageezpz.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static manageezpz.commons.core.Messages.MESSAGE_INVALID_TASK_DISPLAYED_INDEX;
+import static manageezpz.logic.commands.CommandTestUtil.*;
 import static manageezpz.logic.commands.DeleteTaskCommand.MESSAGE_DELETE_TASK_SUCCESS;
+import static manageezpz.logic.commands.DeleteTaskCommand.MESSAGE_USAGE;
 import static manageezpz.testutil.TypicalIndexes.INDEX_FIRST;
 import static manageezpz.testutil.TypicalIndexes.INDEX_SECOND;
 import static manageezpz.testutil.TypicalPersons.ALEX;
 import static manageezpz.testutil.TypicalPersons.BERNICE;
-import static manageezpz.testutil.TypicalTasks.FINISH_CLIENT_PROPOSAL;
-import static manageezpz.testutil.TypicalTasks.MEETING_WITH_CLIENT;
-import static manageezpz.testutil.TypicalTasks.REVIEW_MONTHLY_FINANCE_KPI;
+import static manageezpz.testutil.TypicalTasks.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import manageezpz.commons.core.index.Index;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import manageezpz.model.AddressBook;
@@ -27,8 +29,8 @@ import manageezpz.model.task.Task;
 public class DeleteTaskCommandTest {
     private Model model;
 
-    @Test
-    public void execute_validIndexUnfilteredList_success() {
+    @BeforeEach
+    public void setUp() {
         model = new ModelManager(new AddressBook(), new UserPrefs());
 
         // Add persons to the new address book
@@ -54,7 +56,10 @@ public class DeleteTaskCommandTest {
         model.tagEmployeeToTask(model.getAddressBook().getTaskList().get(2),
                 model.getAddressBook().getPersonList().get(0));
         model.increaseNumOfTasks(model.getAddressBook().getPersonList().get(0));
+    }
 
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
         Task taskToDelete = model.getFilteredTaskList().get(INDEX_FIRST.getZeroBased());
         DeleteTaskCommand deleteTaskCommand = new DeleteTaskCommand(INDEX_FIRST);
 
@@ -64,6 +69,47 @@ public class DeleteTaskCommandTest {
         expectedModel.deleteTask(taskToDelete);
 
         assertCommandSuccess(deleteTaskCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredTaskList().size() + 1);
+
+        DeleteTaskCommand deleteTaskCommand = new DeleteTaskCommand(outOfBoundIndex);
+
+        assertCommandFailure(deleteTaskCommand, model,
+                String.format(MESSAGE_INVALID_TASK_DISPLAYED_INDEX, MESSAGE_USAGE));
+    }
+
+    @Test
+    public void execute_validIndexFilteredList_success() {
+        showTaskAtIndex(model, INDEX_FIRST);
+
+        Task taskToDelete = model.getFilteredTaskList().get(INDEX_FIRST.getZeroBased());
+        DeleteTaskCommand deleteTaskCommand = new DeleteTaskCommand(INDEX_FIRST);
+
+        String expectedMessage = String.format(MESSAGE_DELETE_TASK_SUCCESS, taskToDelete);
+
+        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.deleteTask(taskToDelete);
+        showNoTask(expectedModel);
+
+        assertCommandSuccess(deleteTaskCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexFilteredList_throwsCommandException() {
+        showTaskAtIndex(model, INDEX_FIRST);
+
+        Index outOfBoundIndex = INDEX_SECOND;
+
+        // Ensures that outOfBoundIndex is still in bounds of address book task list
+        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getTaskList().size());
+
+        DeleteTaskCommand deleteTaskCommand = new DeleteTaskCommand(outOfBoundIndex);
+
+        assertCommandFailure(deleteTaskCommand, model,
+                String.format(MESSAGE_INVALID_TASK_DISPLAYED_INDEX, MESSAGE_USAGE));
     }
 
     @Test
